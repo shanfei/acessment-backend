d6555aad086d967bf10e196a60295660e0059edc
shanfei <shanfeishanfei@gmail.com>
2020-10-16 20:30:34 -0700

    fix unittest

--- src/test/java/com/acessment/search/controller/SearchControllerTest.java
+++ src/test/java/com/acessment/search/controller/SearchControllerTest.java
@@ -2,14 +2,17 @@ package com.acessment.search.controller;
 
 import com.acessment.search.model.Employee;
 import com.acessment.search.service.EmployeeService;
-import org.junit.Test;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -39,15 +42,17 @@ public class SearchControllerTest {
     }
 
     @Test
-    void shouldReturnAllEmployees() throws Exception {
+    public void shouldReturnAllEmployees() throws Exception {
 
          String keyword = "alex";
          Date fromDate = new SimpleDateFormat("yyyy-MM-dd").parse("2019-10-22");
          Date endDate = new SimpleDateFormat("yyyy-MM-dd").parse("2020-10-22");
          given(employeeService.searchForEmployee(keyword,fromDate,endDate)).willReturn(this.employeeList);
 
-         this.mockMvc.perform(get("/api/v1/employee/search"))
-                     .andExpect(status().isOk());
+         this.mockMvc.perform(get("/api/v1/employee/search?keyword="+keyword+"&startFrom=2019-10-22&endAt=2020-10-22"))
+                     .andExpect(status().isOk())
+                     .andExpect(jsonPath("$", hasSize(1)));
+
 
     }
 }
